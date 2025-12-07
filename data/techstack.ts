39f9c75d45976661509399f11effc6ddd925ee3f
Jude Oyovbaire <judeoyovbaire@gmail.com>
2025-12-07 20:47:48 +0100

    update technologies

--- data/techstack.ts
+++ data/techstack.ts
@@ -1,6 +1,6 @@
 export const technologies: Record<string, string[]> = {
   'Cloud Platforms': ['AWS', 'Azure', 'GCP'],
-  Orchestration: ['Kubernetes', 'EKS', 'AKS', 'GKE', 'Docker'],
+  Orchestration: ['Kubernetes', 'EKS', 'AKS', 'GKE', 'Docker Swarm'],
   'Infrastructure as Code': ['Terraform', 'Pulumi', 'Crossplane', 'CloudFormation'],
   'GitOps & CI/CD': ['ArgoCD', 'GitHub Actions', 'GitLab CI', 'Azure DevOps', 'Flux'],
   Observability: ['Prometheus', 'Grafana', 'OpenSearch', 'Datadog', 'OpenCost', 'NVIDIA DCGM'],
